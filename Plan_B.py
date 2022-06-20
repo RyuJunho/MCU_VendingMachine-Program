4b8d5c3b28cfde2d92a423d0a97686992c043d84
류준호 <90046151+RyuJunho@users.noreply.github.com>
2022-06-20 20:00:14 +0900

    Add files via upload

--- Plan_B.py
+++ Plan_B.py
@@ -1,7 +1,5 @@
 # MCU_Vending_Machine
 # Client
-
-
 import tkinter as tk
 from tkinter import ttk
 import tkinter.font
@@ -9,7 +7,9 @@ from PIL import ImageTk, Image
 from Sale_DB import *
 from Item_DB import *
 from datetime import datetime, timedelta
+import tkinter.messagebox as msg
 import socket
+
 
 # 바운더리 클래스
 class Main(tk.Tk):
@@ -36,14 +36,19 @@ class Main(tk.Tk):
         left_frame = tk.Frame(self, width=200)
         left_frame.pack(side='left')
 
+        left_label_frame = tk.Frame(left_frame)
+
         # 레이블('재고관리')
-        self.label = tk.Label(left_frame, text='재고관리')
-        self.label.pack(pady=10)
+        self.label = tk.Label(left_label_frame, text='재고관리')
+        self.label.grid(row=0, column=0, padx=50)
 
         # 새로고침 버튼
-        self.repaint_btn = tk.Button(left_frame, text='새로고침'
+        self.repaint_btn = tk.Button(left_label_frame, text='새로고침'
                                      , command=lambda: self.item_repaint(self.itemTable))
-        self.repaint_btn.pack(pady=30)
+        self.repaint_btn.grid(row=0, column=1, padx=50)
+
+        left_label_frame.pack(pady=20)
+
 
         # 테이블과 스크롤바를 부착할 프레임
         item_frame = tk.Frame(left_frame)
@@ -97,21 +102,38 @@ class Main(tk.Tk):
 
         count_change_frame.pack(pady=10)
 
-
+        self.item_repaint(self.itemTable)       #새로고침
 
 
 
         right_frame = tk.Frame(self, width=200)
         right_frame.pack(side='left')
 
+        right_label_frame = tk.Frame(right_frame)
         # 레이블('재고관리')
-        self.label = tk.Label(right_frame, text='거래내역')
-        self.label.pack(pady=10)
+        self.label = tk.Label(right_label_frame, text='거래내역')
+        self.label.grid(row=0, column=0,padx=50)
 
-        #추가 버튼
-        self.repaint_btn = tk.Button(right_frame, text='새로고침'
-                                     , command=lambda: self.sale_repaint(self.Sale_Table,'사과'))
-        self.repaint_btn.pack(pady=30)
+        #새로고침 버튼
+        self.repaint_btn = tk.Button(right_label_frame, text='새로고침'
+                                     , command=lambda: self.sale_repaint(self.Sale_Table))
+        self.repaint_btn.grid(row=0, column=1,padx=50)
+
+        right_label_frame.pack()
+
+
+        right_sale_frame = tk.Frame(right_frame)
+
+        # 엔트리
+        self.sale_entry = tk.Entry(right_sale_frame, width=10, justify='right')
+        self.sale_entry.grid(row=0, column=0, padx=10)
+
+        #구매 버튼
+        self.sale_btn = tk.Button(right_sale_frame, text='구매'
+                                     , command=lambda: self.sale(self.sale_entry.get()))
+        self.sale_btn.grid(row=0, column=1,padx=50)
+
+        right_sale_frame.pack(pady=10)
 
 
         # 테이블과 스크롤바를 부착할 프레임
@@ -135,31 +157,47 @@ class Main(tk.Tk):
         self.Sale_Table.column("two", width=100, )
         self.Sale_Table.heading("two", text="제품명", anchor="center")
 
-        transaction_frame.pack(padx=50,pady=30)  # 검색 결과 테이블 프레임 부착
+        transaction_frame.pack(padx=50,pady=20)  # 검색 결과 테이블 프레임 부착
+
+        self.sale_repaint(self.Sale_Table)    #새로고침
 
         #self.socket_rev()
 
 
     # 선택 버튼 클릭시 값에 따라 프레임의 정보변경
     def modify(self, focus_item, treeview):
-        item_name = treeview.item(focus_item).get('values')[0]
-        self.name_label.configure(text=f'{item_name}')
-        self.count_entry.delete(0, 'end')
+        if focus_item == '' :   #아무것도 선택하지않고 버튼 클릭 시
+            print('에러')
+            # msg.showinfo('에러', '선택된 제품이 없습니다!')
+        else :
+            item_name = treeview.item(focus_item).get('values')[0]
+            self.name_label.configure(text=f'{item_name}')
+            self.count_entry.delete(0, 'end')
 
     #변경 버튼 클릭 시
     def count_modify(self,name_label,count_entry):
-        self.ItemDB = Item_DB()  # Item_DB객체 생성
-        self.Item_df = self.ItemDB.get_df()
-        self.Item_df.loc[name_label['text'],'ItemCount'] = count_entry.get()
-        print(f'{count_entry.get()}으로 값 변경 완료')
-        print(self.Item_df.loc[name_label['text'],'ItemCount'])
-        #msg.showinfo('수량변경 완료', '수량변경이 완료되었습니다!')
+        if name_label['text'] == '선택된 항목 없음' :      #제품을 선택하지 않았을 때
+            print('제품 선택하세요')
+            # msg.showinfo('에러', '제품을 선택하세요!')
+        else :                                          #제품을 선택했을 때
+            if count_entry.get() == '' :                #제품을 선택했지만 값을 입력하지 않았을 때
+                print('값을 입력하세요')
+                # msg.showinfo('에러', '값을 입력하세요!')
+            else :                                      #제품을 선택하고 값을 입력했을 때(정상)
+                self.ItemDB = Item_DB()  # Item_DB객체 생성
+                self.Item_df = self.ItemDB.get_df()
+                print(name_label['text'])
+                self.Item_df.loc[name_label['text'],'ItemCount'] = count_entry.get()
+                print(f'{count_entry.get()}으로 값 변경 완료')
+                print(self.Item_df.loc[name_label['text'],'ItemCount'])
+                #msg.showinfo('수량변경 완료', '수량변경이 완료되었습니다!')
 
-        #파일 저장
-        self.Item_df.to_csv("csv/Item.csv",index=False,encoding='cp949')
-        self.item_repaint(self.itemTable)
+                #파일 저장
+                self.Item_df.to_csv("csv/Item.csv",index=False,encoding='cp949')
+                self.item_repaint(self.itemTable)
 
-        #self.socket_send('사과')
+                #self.socket_send('사과')
+
 
     #재고관리에서 새로고침 클릭 시
     def item_repaint(self,treeview):
@@ -174,21 +212,15 @@ class Main(tk.Tk):
         for index, row in self.Item_df.iterrows():
             treeview.insert('', 'end', text='', value=(row['ItemName'], row['ItemCount'], row['ItemPrice']))
 
+        self.name_label.configure(text=f'선택된 항목 없음')
+        self.count_entry.delete(0, 'end')
+
+
     #거래내역에서 새로고침 클릭 시
-    def sale_repaint(self,treeview,word):
+    def sale_repaint(self,treeview):
         self.SaleDB = Sale_DB()  # Item_DB객체 생성
         self.Sale_df = self.SaleDB.get_df()
-        item_name = word
-        Sale_date = (datetime.today()).strftime("%Y-%m-%d")  # 대여일
-
-        #추가할 데이터프레임
-        Sale_df = pd.DataFrame({'SaleDate': Sale_date, 'SaleName': item_name},index=[0])
-        #데이터 추가
         self.Sale_df = self.Sale_df.dropna()
-        self.Sale_df = pd.concat([self.Sale_df,Sale_df],ignore_index=True)
-
-        #파일 저장
-        self.Sale_df.to_csv("csv/Sale.csv",index=False,encoding='cp949')
 
         #테이블 설정
         # 기존 값 제거
@@ -200,7 +232,8 @@ class Main(tk.Tk):
             treeview.insert('', 'end', text='', value=(row['SaleDate'], row['SaleName']))
 
         #msg.showinfo('새로고침 완료', '새로고침이 완료되었습니다!')
-
+        
+        self.Sale_Table.yview_moveto(1) #표 맨 밑으로 내림
 
     def socket_send(self,word):
         self.client_socket.sendall(word.endcode())
@@ -218,13 +251,32 @@ class Main(tk.Tk):
             print(repr(data.decode()))
             data_list = repr(data.decode()).split(',')
 
-            if data_list[0] == '거래내역' :
 
+            if data_list[0] == '거래내역' :
+                self.Item_df.loc[data_list[1], 'ItemCount'] -= 1    #수량 1감소
                 self.item_repaint(self.itemTable)
                 self.sale_repaint(self.Sale_Table,data_list[1])
 
             self.Item_df.to_csv("csv/Item.csv", index=False, encoding='cp949')
             self.Sale_df.to_csv("csv/Sale.csv", index=False, encoding='cp949')
+
+
+    def sale(self,item_name):
+        self.Item_df.loc[item_name, 'ItemCount'] -= 1  # 수량 1감소
+        if (self.Item_df.loc[item_name, 'ItemCount'] == 0 ) :
+            print('제품이 다떨어졌습니다.')
+            msg.showinfo('수량없음', '제품이 모두 소진되었습니다.')
+            #self.socket_send(item_name)    #제품 이름 전송
+        self.Item_df.to_csv("csv/Item.csv",index=False,encoding='cp949')    #파일저장
+        self.item_repaint(self.itemTable)   #제품창 새로고침
+
+        Sale_date = (datetime.today()).strftime("%Y-%m-%d")  # 대여일
+        #추가할 데이터프레임
+        Sale_df = pd.DataFrame({'SaleDate': Sale_date, 'SaleName': item_name},index=[0])
+        #데이터 추가
+        self.Sale_df = pd.concat([self.Sale_df,Sale_df],ignore_index=True)
+        self.Sale_df.to_csv("csv/Sale.csv",index=False,encoding='cp949')    #파일저장
+        self.sale_repaint(self.Sale_Table)   #판매창 새로고침
 
 
 
